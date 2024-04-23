9fd6aa0367aec6d4c01f59c80ae765681c74804c
maximelopez <maxime.lpz@outlook.fr>
2024-04-23 17:57:05 +0200

    routes users

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,6 @@ router.post("/signup", usersCtrl.signup);
 router.post("/signin", usersCtrl.signin);
 router.delete("/remove/:token", usersCtrl.remove);
 router.put("/modify/:token", usersCtrl.modify);
-router.get("/print/:token", usersCtrl.print);
 
 router.put("/like/:token/:eventId", usersCtrl.likeEvent);
 router.get("/like/:token", usersCtrl.loadEvent);
