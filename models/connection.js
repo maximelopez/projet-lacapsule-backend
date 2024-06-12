79b05fd9a73951950f6da2ad5a77ac3e4a5369d2
maximelopez <maxime.lpz@outlook.fr>
2024-06-12 15:55:14 +0200

    variable env

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb+srv://admin:HcRhE7A0FoNjqAwm@cluster0.30bx91i.mongodb.net/appcertification";
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
